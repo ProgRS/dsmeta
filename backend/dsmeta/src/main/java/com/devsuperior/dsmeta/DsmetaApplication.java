36260fdcfde7c77ad4d623f5cca0b649752715a6
ProgRs <lfernandotc@hotmail.com>
2022-07-13 10:02:52 -0300

    Security config

--- backend/dsmeta/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/dsmeta/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsmovie;
+package com.devsuperior.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
